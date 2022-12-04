f8d2c78073aa73aa8667a71bd5671bb596fae5e0
Otávio Silva <otaviosilva2632@gmail.com>
2022-12-04 18:21:16 -0300

    feat: refactoring code and add more tests case

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,6 +1,11 @@
 const queryString = obj =>
   Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => {
+      if (typeof value === 'object' && !Array.isArray(value))
+        throw new Error('Please check your params');
+
+      return `${key}=${value}`;
+    })
     .join('&');
 
 module.exports = {
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,6 +1,6 @@
 const { queryString } = require('./queryString');
 
-describe('Objeto to query string', () => {
+describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'otavio',
@@ -18,5 +18,26 @@ describe('Objeto to query string', () => {
     // queryString(obj);
     // in this case, we have a comparision between `name=otavio&job=dev` and `name=otavio*job=dev`
     expect(queryString(obj)).not.toBe('name=otavio*job=dev');
+  });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'otavio',
+      abilities: ['JS', 'CSHARP'],
+    };
+    expect(queryString(obj)).toBe('name=otavio&abilities=JS,CSHARP');
+  });
+
+  it('should throw an error when an object an array is passed as value', () => {
+    const obj = {
+      name: 'otavio',
+      abilities: {
+        first: 'JS',
+        second: 'CSHARP',
+      },
+    };
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
