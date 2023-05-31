4437d8fabd45638628bd522a486c628846507aaa
Otávio Silva <otaviosilva2632@gmail.com>
2023-05-30 23:27:28 -0300

    test: add one item for time using lodash

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,14 +1,21 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
 export default class Cart {
   items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+    this.items.push(item);
+  }
 
   getTotal() {
     return this.items.reduce((acc, item) => {
       return acc + item.quantity * item.product.price;
     }, 0);
-  }
-
-  add(item) {
-    this.items.push(item);
   }
 
   remove(product) {}
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,9 +2,14 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product;
 
   beforeEach(() => {
     cart = new Cart();
+    product = {
+      title: 'Adidas Running - Men',
+      price: 35388, // 353.88 | R$ 353,88
+    };
   });
 
   it('should return 0 when getTotal() is executed in a newly created instance ', () => {
@@ -13,15 +18,26 @@ describe('Cart', () => {
 
   it('should multiply quantity and price and recieve the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas Running - Men',
-        price: 35388, // 353.88 | R$ 353,88
-      },
+      product,
       quantity: 2, // 70776
     };
 
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
   });
 });
