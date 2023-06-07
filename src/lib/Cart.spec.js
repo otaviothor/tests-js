82480f3caf36e97a99addcd7abf18e2f5015d1cf
Otávio Silva <otaviosilva2632@gmail.com>
2023-06-07 20:55:46 -0300

    test: implemente second discount condition in getTotal() when quantity is odd

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -128,6 +128,21 @@ describe('Cart', () => {
       expect(cart.getTotal().getAmount()).toEqual(74315);
     });
 
+    it('should not apply percentage discount quantity is below or equals minimum', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 2,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
     it('should apply quantity discount for even quantities', () => {
       const condition = {
         quantity: 2,
@@ -140,6 +155,34 @@ describe('Cart', () => {
       });
 
       expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should not apply quantity discount for even quantities when condition is not met', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(35388);
+    });
+
+    it('should apply quantity discount for odd quantities', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 5,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(106164);
     });
   });
 });
